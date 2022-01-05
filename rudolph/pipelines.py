95ca857a574f61488962dd96fb6b8042a661d0ba
shonenkov <shonenkov@phystech.edu>
2022-01-06 00:35:05 +0300

    zs clf

--- rudolph/pipelines.py
+++ rudolph/pipelines.py
@@ -10,6 +10,7 @@ import more_itertools
 import numpy as np
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+import torchvision.transforms as T
 from tqdm.auto import tqdm
 from einops import rearrange
 
@@ -155,3 +156,88 @@ def self_reranking(
                 )).mean(-1)
             )
     return torch.cat(ppl_text), torch.cat(ppl_image)
+
+
+def zs_clf(pil_img, classes, model, tokenizer, vae, template=''):
+    """
+    classes - list of strings
+    template - prefix template
+    """
+    vocab_size = model.get_param('vocab_size')
+    image_tokens_per_dim = model.get_param('image_tokens_per_dim')
+    l_text_seq_length = model.get_param('l_text_seq_length')
+    r_text_seq_length = model.get_param('r_text_seq_length')
+    image_seq_length = model.get_param('image_seq_length')
+    device = model.get_param('device')
+
+    image_transform = T.Compose([
+        T.Lambda(lambda img: img.convert('RGB') if img.mode != 'RGB' else img),
+        T.RandomResizedCrop(image_tokens_per_dim * 8,
+                            scale=(1., 1.),
+                            ratio=(1., 1.)),
+        T.ToTensor()
+    ])
+
+    template = template.lower().strip()
+    template_encoded = tokenizer.encode_text(template, text_seq_length=r_text_seq_length)
+    template_size = (template_encoded != 0).sum() - 2  # bos, eos
+    template_encoded = template_encoded[:template_size + 1]
+
+    encoded, masks = [], []
+    for _class in classes:
+        _class = _class.lower().strip()
+        class_encoded = tokenizer.encode_text(f' {_class}', text_seq_length=r_text_seq_length)
+        if template_size:
+            class_encoded = torch.cat([template_encoded, class_encoded[1:-template_size]])
+        encoded.append(class_encoded)
+
+        mask = torch.zeros(r_text_seq_length, dtype=torch.int64)
+        mask[class_encoded != 0] = 1
+        masks.append(mask)
+
+    encoded = torch.stack(encoded, 0)
+    masks = torch.stack(masks, 0)
+
+    bs = len(classes)
+
+    with torch.no_grad():
+        attention_mask = get_i2t_attention_mask(masks, bs, l_text_seq_length, image_tokens_per_dim, r_text_seq_length,
+                                                device)
+        img = image_transform(pil_img)
+        images = img.unsqueeze(0).to(device)
+        image_input_ids = vae.get_codebook_indices(images)
+
+        input_ids = torch.cat((
+            torch.zeros(l_text_seq_length, dtype=torch.int64).repeat(bs, 1).to(device),
+            image_input_ids.repeat(bs, 1),
+            encoded.to(device),
+        ), dim=1)
+
+        logits, _ = model(input_ids, attention_mask, has_cache=False, use_cache=False, return_loss=False)
+        logits = rearrange(logits, 'b n c -> b c n')
+
+        image_logits = logits[:, vocab_size:,
+                              l_text_seq_length:l_text_seq_length + image_seq_length - 1].contiguous()  # .float()
+        r_text_logits = logits[:, :vocab_size, -r_text_seq_length:-1].contiguous()
+
+        ppl_text = torch.exp(F.cross_entropy(
+            r_text_logits[:, :, template_size:],
+            input_ids[:, -(r_text_seq_length - 1 - template_size):],
+            ignore_index=0,
+            reduction='none',
+        )).mean(-1)
+
+        ppl_image = torch.exp(F.cross_entropy(
+            image_logits,
+            input_ids[:, l_text_seq_length + 1:l_text_seq_length + image_seq_length],
+            reduction='none',
+        )).mean(-1)
+
+        pred = ppl_text.argmin().item()
+
+    return {
+        'label': pred,
+        'class': classes[pred],
+        'ppl_text': ppl_text.cpu().numpy(),
+        'ppl_image': ppl_image.cpu().numpy(),
+    }
